08dd868ebee1a3432d47eb6214fe175b922f86c1
Luma <world@luma.email>
2022-03-09 11:26:00 +0900

    fix type check

--- src/constants/ipc.ts
+++ src/constants/ipc.ts
@@ -2,4 +2,5 @@ export const IPC_CONFIG_NAME = 'notios';
 export const envVarNames = {
   rootToken: 'NOTIOS_ROOT_TOKEN',
   parentToken: 'NOTIOS_PARENT_TOKEN',
+  doNotAlternate: 'NOTIOS_DO_NOT_ALTERNATE',
 };
