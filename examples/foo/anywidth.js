e595e171d7a94b5040edd5035e12d21db56354ac
Luma <world@luma.email>
2022-03-10 22:37:27 +0900

    add picocolors example

--- examples/foo/anywidth.js
+++ examples/foo/anywidth.js
@@ -4,3 +4,4 @@ console.log('abcdefghijklmnopqrstuvxyz'.repeat(5));
 console.log('あういえおかきくけこ😁😂😃😅'.repeat(5));
 // ambiguous
 console.log('¡¤§¨ª­®°±²³´¶·¸¹º¼½¾¿ÆÐ×ØÞßàá'.repeat(5));
+console.log('abc abc'.repeat(5));
