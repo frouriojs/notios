9a169e7cc9bacfd77ef3204c6c4504d92985320f
Luma <world@luma.email>
2022-03-14 12:12:56 +0900

    add examples

---
+++ examples/foo/info.js
@@ -0,0 +1,4 @@
+console.log({
+  'process.stdout.isTTY': process.stdout.isTTY,
+  'process.stdout.getColorDepth()': process.stdout.getColorDepth?.(),
+});
--- examples/foo/lines.js
+++ examples/foo/lines.js
@@ -1,4 +1,5 @@
 const n = Number.parseInt(process.argv[2], 10);
+
 Array(n)
   .fill(0)
   .forEach((_, i) => {
