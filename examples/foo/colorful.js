cb1ba81c693cc516d9da805e874ddaa5e7f095f0
Luma <luma@world.email>
2022-04-14 10:44:29 +0000

    feat: add example

---
+++ examples/foo/colorful.js
@@ -0,0 +1,13 @@
+const delay = (ms) => new Promise((r) => setTimeout(r, ms));
+const main = async () => {
+  console.log('starting colorful1!');
+  console.log('starting colorful2!');
+  console.log('starting colorful3!');
+  console.log('starting colorful4!');
+  // await delay(1000);
+  // console.log('\x1b[33maaaa');
+  // await delay(1000);
+  // console.log('\x1b[33maaaa');
+};
+
+main();
